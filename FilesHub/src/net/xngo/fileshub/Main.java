4bd08a01f19d3b6adb60cd8583b7c65aca8e0a5d
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-07-17 11:54:48 -0400

    Call Hub to run Database.java

--- FilesHub/src/net/xngo/fileshub/Main.java
+++ FilesHub/src/net/xngo/fileshub/Main.java
@@ -3,12 +3,19 @@ package net.xngo.fileshub;
 import java.io.File;
 import java.util.ArrayList;
 
+import net.xngo.fileshub.Hub;
+
+/**
+ * 
+ * @author Xuan Ngo
+ *
+ */
 public class Main
 {
 
   public static void main(String[] args)
   {
-
+    Hub hub = new Hub();
 
   }
  
