15e406547b4aef4cd7da1ca311545c21dbdc9bc6
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-07-17 11:55:18 -0400

    Call database.java

--- FilesHub/src/net/xngo/fileshub/Hub.java
+++ FilesHub/src/net/xngo/fileshub/Hub.java
@@ -1,6 +1,20 @@
 package net.xngo.fileshub;
 
+import java.io.File;
+
+import net.xngo.fileshub.Database;
+
+/**
+ * 
+ * @author Xuan Ngo
+ *
+ */
 public class Hub
 {
-
+  private Database database = new Database();
+  
+  public boolean add(File file)
+  {
+    return true;
+  }
 }
