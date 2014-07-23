b44a7f47333d2ff44383eb8b4fad80928704ab2a
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-07-23 17:06:26 -0400

    Duplicate table implemented.

--- FilesHub/test/test.FilesHub.bat
+++ FilesHub/test/test.FilesHub.bat
@@ -1,6 +1,8 @@
 REM sqlite3 FilesHub.db -column -header "SELECT * FROM Document;"
 REM sqlite3 FilesHub.db "SELECT * FROM Document;"
 
-sqlite3 FilesHub.db "SELECT filename FROM Document ORDER BY filename ASC;"
+REM sqlite3 FilesHub.db "SELECT filename FROM Document ORDER BY filename ASC;"
 
 sqlite3 FilesHub.db -cmd ".width 6 90 25" -column -header "SELECT uid, canonical_path, hash FROM Document;"
+
+sqlite3 FilesHub.db -cmd ".width 3 4 90" -column -header "SELECT uid, duid, canonical_path FROM Duplicate;"
