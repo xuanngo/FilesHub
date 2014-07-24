abf571fdb5760e4e1ebb71de51058daf5e08272a
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-07-24 13:25:40 -0400

    Increase width to view the full canonical path.

--- FilesHub/test/test.FilesHub.bat
+++ FilesHub/test/test.FilesHub.bat
@@ -5,6 +5,6 @@ REM sqlite3 FilesHub.db "SELECT filename FROM Document ORDER BY filename ASC;"
 
 cls
 
-sqlite3 FilesHub.db -cmd ".width 4 15 90" -column -header "SELECT uid, hash, canonical_path FROM Document;"
+sqlite3 FilesHub.db -cmd ".width 4 15 125" -column -header "SELECT uid, hash, canonical_path FROM Document;"
 
-sqlite3 FilesHub.db -cmd ".width 4 15 4 90" -column -header "SELECT duid, hash, uid, canonical_path FROM Duplicate;"
+sqlite3 FilesHub.db -cmd ".width 4 15 4 125" -column -header "SELECT duid, hash, uid, canonical_path FROM Duplicate;"
