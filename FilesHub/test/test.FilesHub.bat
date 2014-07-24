dc8e00876ded29006aed72809f21f7abf001c6d9
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-07-24 11:34:10 -0400

    Add hash column in SELECT.

--- FilesHub/test/test.FilesHub.bat
+++ FilesHub/test/test.FilesHub.bat
@@ -3,6 +3,8 @@ REM sqlite3 FilesHub.db "SELECT * FROM Document;"
 
 REM sqlite3 FilesHub.db "SELECT filename FROM Document ORDER BY filename ASC;"
 
-sqlite3 FilesHub.db -cmd ".width 6 90 25" -column -header "SELECT uid, canonical_path, hash FROM Document;"
+cls
 
-sqlite3 FilesHub.db -cmd ".width 3 4 90" -column -header "SELECT uid, duid, canonical_path FROM Duplicate;"
+sqlite3 FilesHub.db -cmd ".width 6 90 25" -column -header "SELECT uid, hash, canonical_path,  FROM Document;"
+
+sqlite3 FilesHub.db -cmd ".width 3 4 90" -column -header "SELECT uid, duid, hash, canonical_path FROM Duplicate;"
