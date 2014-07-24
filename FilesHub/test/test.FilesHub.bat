1436dafb8cbce181854de593c5e1277c8a0fa88e
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-07-24 11:39:09 -0400

    Fix display columns width.

--- FilesHub/test/test.FilesHub.bat
+++ FilesHub/test/test.FilesHub.bat
@@ -5,6 +5,6 @@ REM sqlite3 FilesHub.db "SELECT filename FROM Document ORDER BY filename ASC;"
 
 cls
 
-sqlite3 FilesHub.db -cmd ".width 6 90 25" -column -header "SELECT uid, hash, canonical_path,  FROM Document;"
+sqlite3 FilesHub.db -cmd ".width 4 15 90" -column -header "SELECT uid, hash, canonical_path FROM Document;"
 
-sqlite3 FilesHub.db -cmd ".width 3 4 90" -column -header "SELECT uid, duid, hash, canonical_path FROM Duplicate;"
+sqlite3 FilesHub.db -cmd ".width 4 15 4 90" -column -header "SELECT duid, hash, uid, canonical_path FROM Duplicate;"
