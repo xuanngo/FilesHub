a894ee62b21101c988896ef0bb3bb947474f1cd2
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-08-04 14:38:19 -0400

    Fix Shelf table.

--- FilesHub/test/test.FilesHub.bat
+++ FilesHub/test/test.FilesHub.bat
@@ -1,10 +1,10 @@
-REM sqlite3 FilesHub.db -column -header "SELECT * FROM Repository;"
-REM sqlite3 FilesHub.db "SELECT * FROM Repository;"
+REM sqlite3 FilesHub.db -column -header "SELECT * FROM Shelf;"
+REM sqlite3 FilesHub.db "SELECT * FROM Shelf;"
 
-REM sqlite3 FilesHub.db "SELECT filename FROM Repository ORDER BY filename ASC;"
+REM sqlite3 FilesHub.db "SELECT filename FROM Shelf ORDER BY filename ASC;"
 
 cls
 
-sqlite3 FilesHub.db -cmd ".width 4 15 15 125" -column -header "SELECT uid, hash, last_modified, canonical_path FROM Repository;"
+sqlite3 FilesHub.db -cmd ".width 4 15 15 125" -column -header "SELECT uid, hash, last_modified, canonical_path FROM Shelf;"
 
 sqlite3 FilesHub.db -cmd ".width 4 15 15 4 125" -column -header "SELECT duid, hash, last_modified, uid, canonical_path FROM Trash;"
