d580b5d652e27494ead00be3a428ac6abf5e0a02
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-08-07 12:48:34 -0400

    Add more statuses to handle different cases.

--- FilesHub/src/net/xngo/fileshub/struct/ResultDocSet.java
+++ FilesHub/src/net/xngo/fileshub/struct/ResultDocSet.java
@@ -11,11 +11,12 @@ public class ResultDocSet
 {
   // Possible statuses.
   private static int i=0;
-  public final static int EXACT_SAME_FILE     = i++;  // Exact same file.
-  public final static int SAME_PATH_DIFF_HASH = i++;  // File has changed.
-  public final static int DIFF_PATH_DIFF_HASH = i++;  // New unique file.
-  public final static int DIFF_PATH_SAME_HASH = i++;  // Duplicate file.
-  
+  public final static int EXACT_SAME_FILE       = i++;  // Exact same file.
+  public final static int SAME_PATH_DIFF_HASH   = i++;  // File has changed.
+  public final static int DIFF_PATH_DIFF_HASH   = i++;  // New unique file.
+  public final static int DIFF_PATH_SAME_HASH   = i++;  // Duplicate file.
+  public final static int EXACT_SAME_TRASH_FILE = i++;  // Exact same file that you deleted.
+  public final static int SAME_TRASH_PATH_DIFF_HASH = i++;  // Exact same file.
   
   public int      status   = -1;
   public File     file     = null; // Original file to add.
