3ab5c506790127edeb09f4c9584031734deb171d
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-08-13 16:01:43 -0400

    Add getStatusMsg() for readability.

--- FilesHub/src/net/xngo/fileshub/struct/ResultDocSet.java
+++ FilesHub/src/net/xngo/fileshub/struct/ResultDocSet.java
@@ -11,15 +11,32 @@ public class ResultDocSet
 {
   // Possible statuses.
   private static int i=0;
-  public final static int EXACT_SAME_FILE       = i++;  // Exact same file.
-  public final static int SAME_PATH_DIFF_HASH   = i++;  // File has changed.
-  public final static int DIFF_PATH_DIFF_HASH   = i++;  // New unique file.
-  public final static int DIFF_PATH_SAME_HASH   = i++;  // Duplicate file.
-  public final static int EXACT_SAME_TRASH_FILE = i++;  // Exact same file that you deleted.
-  public final static int SAME_TRASH_PATH_DIFF_HASH = i++;  // Exact same file.
+  public static final int EXACT_SAME_FILE       = i++;  // Exact same file.
+  public static final int SAME_PATH_DIFF_HASH   = i++;  // File has changed.
+  public static final int DIFF_PATH_DIFF_HASH   = i++;  // New unique file.
+  public static final int DIFF_PATH_SAME_HASH   = i++;  // Duplicate file.
+  public static final int EXACT_SAME_TRASH_FILE = i++;  // Exact same file that you deleted.
+  public static final int SAME_TRASH_PATH_DIFF_HASH = i++;  // Exact same file.
   
   public int      status   = -1;
   public File     file     = null; // Original file to add.
   public Document document = null; // The 'document' that conflicts with 'file'. The document that influences the status.
 
+  public String getStatusMsg()
+  {
+    if(this.status==EXACT_SAME_FILE)
+      return "Exact same file";
+    else if (this.status==SAME_PATH_DIFF_HASH)
+      return "File has changed";
+    else if (this.status==DIFF_PATH_DIFF_HASH)
+      return "New unique file";
+    else if (this.status==DIFF_PATH_SAME_HASH)
+      return "Duplicate file";
+    else if (this.status==EXACT_SAME_TRASH_FILE)
+      return "Exact same duplicated file";
+    else if (this.status==SAME_TRASH_PATH_DIFF_HASH)
+      return "Exact same duplicated file has changed";
+    else
+      return "Unknown status";
+  }
 }
