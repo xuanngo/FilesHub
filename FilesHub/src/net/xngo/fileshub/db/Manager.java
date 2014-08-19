d96ffe03f2925181e8131d775f0007d83ba8dcb7
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-08-19 10:32:57 -0400

    New logic of addFile(): Use filename instead of path.

--- FilesHub/src/net/xngo/fileshub/db/Manager.java
+++ FilesHub/src/net/xngo/fileshub/db/Manager.java
@@ -67,7 +67,7 @@ public class Manager
    * @param file
    * @return
    */
-  public ResultDocSet addFile(File file)
+  public ResultDocSet addFile_old(File file)
   {
     ResultDocSet resultDocSet = new ResultDocSet();
     resultDocSet.file         = file;
@@ -171,6 +171,139 @@ public class Manager
     }
     
     return resultDocSet;    
+  }
+  /**
+   * Goal: Avoid processing hash multiple times:
+   * If name found in Shelf
+   *    If last modified time found
+   *        Return Duplicate
+   *    else
+   *        If exact file path found in Shelf
+   *            -Copy Shelf document to Trash.
+   *            -Update Shelf document with current file.
+   *            -Return New File(File content has changed.)
+   *        else
+   *            If last modifed time found in Trash
+   *                Return Duplicate
+   *            else
+   *                -Add file info in Trash(New Hash)
+   *                -Return Duplicate
+   * else
+   *    If name found in Trash
+   *        If last modified time found
+   *            Return Duplicate
+   *        else
+   *            -Add file info in Trash(New hash)
+   *            -Return Duplicate
+   *    else
+   *        hash = getHash(file)
+   *        If hash found in Shelf
+   *            -Add file info in Trash(New name)
+   *            -Return Duplicate
+   *        else
+   *            If hash found in Trash
+   *                -Add file info in Trash(New name)
+   *                -Return Duplicate
+   *            else
+   *                -Add file info in Shelf
+   *                -Return New File(Completely new file)
+   * @param file
+   * @return  Existing and conflicting document. Otherwise, null.
+   */
+  public Document addFile(File file)
+  {
+    final String filename = file.getName();
+    final long lastModified = file.lastModified();
+    
+    Document shelfDoc = shelf.findDocByFilename(filename);
+    if(shelfDoc != null)
+    {
+      if(shelfDoc.last_modified == lastModified)
+        return shelfDoc;
+      else
+      {
+        Document tmpShelfDoc = shelf.findDocByCanonicalPath(Utils.getCanonicalPath(file));
+        if(tmpShelfDoc != null)
+        {
+          trash.addDoc(tmpShelfDoc);
+          
+          Document newShelfDoc = new Document(file);
+          newShelfDoc.uid = tmpShelfDoc.uid;
+          newShelfDoc.hash = Utils.getHash(file);
+          shelf.saveDoc(newShelfDoc);
+          
+          return null; // Dilemma null or newShelfDoc. In this case, it's the exact same file and path but content has changed.
+        }
+        else
+        {
+          Document trashDoc = trash.findDocByModifiedTimeAndFilename(lastModified, filename);
+          if(trashDoc != null)
+            return shelfDoc;
+          else
+          {
+            Document newTrashDoc = new Document(file);
+            newTrashDoc.uid = shelfDoc.uid;
+            newTrashDoc.hash = Utils.getHash(file);
+            trash.addDoc(newTrashDoc);
+  
+            return shelfDoc;
+          }
+        }
+      }
+    }
+    else
+    {
+      Document trashDoc = trash.findDocByFilename(filename);
+      if(trashDoc != null)
+      {
+        if(trash.findDocByModifiedTimeAndFilename(lastModified, filename) != null)
+        {
+          return shelf.findDocByUid(trashDoc.uid);
+        }
+        else
+        {
+          Document newTrashDoc = new Document(file);
+          newTrashDoc.uid = trashDoc.uid;
+          newTrashDoc.hash = Utils.getHash(file);
+          trash.addDoc(newTrashDoc);
+          return shelf.findDocByUid(trashDoc.uid);
+        }
+      }
+      else
+      {
+        final String hash = Utils.getHash(file);
+        shelfDoc = shelf.findDocByHash(hash);
+        if(shelfDoc != null)
+        {
+          Document newTrashDoc = new Document(file);
+          newTrashDoc.uid = shelfDoc.uid;
+          newTrashDoc.hash = hash;
+          trash.addDoc(newTrashDoc);
+          return shelfDoc;
+        }
+        else
+        {
+          trashDoc = trash.findDocByHash(hash);
+          if(trashDoc != null)
+          {
+            Document newTrashDoc = new Document(file);
+            newTrashDoc.uid = trashDoc.uid;
+            newTrashDoc.hash = hash;
+            trash.addDoc(newTrashDoc);
+            return shelf.findDocByUid(trashDoc.uid);
+          }
+          else
+          {
+            Document newShelfDoc = new Document(file);
+            newShelfDoc.hash = hash;
+            shelf.addDoc(newShelfDoc);
+            
+            return null;
+          }
+        }
+        
+      }
+    }
   }
   
   /**
