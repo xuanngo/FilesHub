d50b90e35de7c3b53d9004c0f429d4a87ac2cceb
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-09-05 15:16:20 -0400

    Hope to fix unicode in filename not found.

--- FilesHub/releases/latest/FilesHub.bat
+++ FilesHub/releases/latest/FilesHub.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 SET FILESHUB_HOME=%~dp0
 @ECHO ON
-java -DFilesHub.home=%FILESHUB_HOME% -jar %FILESHUB_HOME%fileshub.jar %*
+java -Dfile.encoding=UTF-8 -DFilesHub.home=%FILESHUB_HOME% -jar %FILESHUB_HOME%fileshub.jar %*
