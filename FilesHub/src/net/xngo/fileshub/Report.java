a9f5c46baa0638d192bec1e2dcab651960700d6d
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-09-10 15:33:51 -0400

    Implement sorting.

--- FilesHub/src/net/xngo/fileshub/Report.java
+++ FilesHub/src/net/xngo/fileshub/Report.java
@@ -3,10 +3,13 @@ package net.xngo.fileshub;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Collections;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.BufferedWriter;
+
 
 import org.supercsv.io.CsvListWriter;
 import org.supercsv.io.ICsvListWriter;
@@ -15,36 +18,44 @@ import org.supercsv.cellprocessor.ift.CellProcessor;
 import org.supercsv.cellprocessor.constraint.NotNull;
 
 import net.xngo.fileshub.struct.Document;
+import net.xngo.fileshub.struct.Duplicate;
 
 public class Report
 {
-  private ArrayList<Document> toAddDocs = new ArrayList<Document>();
-  private ArrayList<Document> existingDocs = new ArrayList<Document>();
   
-  public void addDuplicate(Document toAddDoc, Document existingDoc)
+  private ArrayList<Duplicate> duplicates = new ArrayList<Duplicate>();
+  
+  
+  public void addDuplicate(Document toAddDoc, Document shelfDoc)
   {
-    this.toAddDocs.add(toAddDoc);
-    this.existingDocs.add(existingDoc);
+    this.duplicates.add(new Duplicate(toAddDoc, shelfDoc));
+  }
+  
+  public void sort()
+  {
+    // Sort
+    Collections.sort(this.duplicates);
   }
   
   public void display()
   {
-    if(this.toAddDocs.size()>0)
+    if(this.duplicates.size()>0)
     {
+
       System.out.println("Duplicate files:");
       System.out.println("=================");
       
       // Get total size.
       long totalSize = 0;
-      for(Document toAddDoc: this.toAddDocs)
+      for(Duplicate dup: this.duplicates)
       {
-        File file = new File(toAddDoc.canonical_path);
+        File file = new File(dup.toAddDoc.canonical_path);
         totalSize += file.length();
-        System.out.println(toAddDoc.canonical_path);
+        System.out.println(dup.toAddDoc.canonical_path);
       }
       
       System.out.println("========================================================");
-      System.out.println(String.format("Total size of %s duplicate files = %s.", toAddDocs.size(), Utils.readableFileSize(totalSize)));
+      System.out.println(String.format("Total size of %s duplicate files = %s.", this.duplicates.size(), Utils.readableFileSize(totalSize)));
     }
     else
       System.out.println("There is no duplicate file.");
@@ -64,9 +75,9 @@ public class Report
       listWriter.writeHeader(header);
       
       // write
-      for(int i=0; i<toAddDocs.size(); i++)
+      for(int i=0; i<this.duplicates.size(); i++)
       {
-        final List<Object> row = Arrays.asList(new Object[] { this.toAddDocs.get(i).canonical_path, this.existingDocs.get(i).canonical_path});            
+        final List<Object> row = Arrays.asList(new Object[] { this.duplicates.get(i).toAddDoc.canonical_path, this.duplicates.get(i).shelfDoc.canonical_path});            
         listWriter.write(row, processors);
       }
             
@@ -105,11 +116,11 @@ public class Report
       BufferedWriter toDeleteFileExecutableBuffer = new BufferedWriter(toDeleteExecutable);
       BufferedWriter fromDatabaseFileBuffer = new BufferedWriter(fromDatabaseFile);
       
-      for(int i=0; i<this.toAddDocs.size(); i++)
+      for(int i=0; i<this.duplicates.size(); i++)
       {
-        toDeleteFileExecutableBuffer.write(this.printDelete(this.toAddDocs.get(i).canonical_path)+"\n");
-        toDeleteFileBuffer.write(this.toAddDocs.get(i).canonical_path+"\n");
-        fromDatabaseFileBuffer.write(this.existingDocs.get(i).canonical_path+"\n");
+        toDeleteFileExecutableBuffer.write(this.printDelete(this.duplicates.get(i).toAddDoc.canonical_path)+"\n");
+        toDeleteFileBuffer.write(this.duplicates.get(i).toAddDoc.canonical_path+"\n");
+        fromDatabaseFileBuffer.write(this.duplicates.get(i).shelfDoc.canonical_path+"\n");
       } 
       
       toDeleteFileBuffer.close();
