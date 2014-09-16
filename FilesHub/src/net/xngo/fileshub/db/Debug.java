97c9395aff5238a180c43210ea60795802412c0a
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-09-16 12:00:50 -0400

    Turn activate() to public.

--- FilesHub/src/net/xngo/fileshub/db/Debug.java
+++ FilesHub/src/net/xngo/fileshub/db/Debug.java
@@ -4,15 +4,21 @@ public class Debug
 {
   private static boolean debug = false;
   
-
-  
+  /**
+   * Display debug message.
+   * @param s
+   */
   public static void msg(String s)
   {
     if(Debug.activate())
       System.out.println("DEBUG: "+s);
   }
   
-  private static boolean activate()
+  /**
+   * 
+   * @return True is debug mode is ON. Otherwise, false.
+   */
+  public static boolean activate()
   {
     String sDebug = System.getProperty(Conn.DB_NAME+".debug");
     if(sDebug!=null)
