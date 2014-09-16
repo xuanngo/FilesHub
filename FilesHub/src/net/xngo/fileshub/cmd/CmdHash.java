577777d39e62fa57da9b8f79156ab3608393687b
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-09-16 11:56:36 -0400

    Fix: For unknown reason, JCommander doesn't allow me to use Set<File>. Therefore, change to List<File>.

--- FilesHub/src/net/xngo/fileshub/cmd/CmdHash.java
+++ FilesHub/src/net/xngo/fileshub/cmd/CmdHash.java
@@ -2,6 +2,7 @@ package net.xngo.fileshub.cmd;
 
 import java.io.File;
 import java.util.Set;
+import java.util.List;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
@@ -14,10 +15,10 @@ public class CmdHash
   public static String name = "hash";
   
   @Parameter(description = "Hash list of files or directories.", variableArity = true)
-  public Set<File> addPaths;
+  public List<File> paths;
   
   public final Set<File> getAllUniqueFiles()
   {
-    return FileUtils.listFiles(this.addPaths);
+    return FileUtils.listFiles(this.paths);
   }    
 }
