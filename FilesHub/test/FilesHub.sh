c9621b7135cca0a7f52c911e1b9384ff640aa174
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-09-18 15:24:28 -0400

    Add template.html. Update libraries.

--- FilesHub/test/FilesHub.sh
+++ FilesHub/test/FilesHub.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-
-# Get the path location of the executing script
-## http://stackoverflow.com/questions/630372/determine-the-path-of-the-executing-bash-script
-FILESHUB_HOME="`dirname \"$0\"`"                    # relative
-FILESHUB_HOME="`( cd \"$FILESHUB_HOME\" && pwd )`"  # absolutized and normalized
-if [ -z "$FILESHUB_HOME" ] ; then
-  # error; for some reason, the path is not accessible
-  # to the script (e.g. permissions re-evaled after suid)
-  exit 1  # fail
-fi
-
-## ISO-8859-1
-java -Dfile.encoding=UTF-8 -DFilesHub.home=${FILESHUB_HOME} -jar ${FILESHUB_HOME}/fileshub.jar "$@"
+C:\temp\Xuan\bckable\work\github\FilesHub\FilesHub\test\FilesHub.sh
