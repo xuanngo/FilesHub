7186559998a97838fe3bede5c9ea00992888db2b
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-09-30 15:48:36 -0400

    Refactor method names: getXXXX() is for exact criteria whereas searchXXXX() is not, e.g. wildcard(*)

---
+++ FilesHub/test/ng.bat
@@ -0,0 +1,15 @@
+REM Description: Build and run testng according to testng-fileshub.xml.
+REM Author: Xuan Ngo
+REM Usage: ng.bat
+
+SET FILESHUB_TEST=%~dp0
+CD %FILESHUB_TEST%
+CD ..
+REM CALL build.bat
+CD %FILESHUB_TEST%
+CLS
+
+java -DFilesHub.files.in.batch=11 -DFilesHub.home=%FILESHUB_TEST% -cp "..\lib\*;..\bin" org.testng.TestNG testng-fileshub.xml
+
+
+CALL test.FilesHub.bat
