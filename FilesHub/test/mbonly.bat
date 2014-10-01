76f35535995c43375c3f2ff1a5fde6bc9d2ce1be
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-10-01 16:31:28 -0400

    Remove '-e'

--- FilesHub/test/mbonly.bat
+++ FilesHub/test/mbonly.bat
@@ -5,4 +5,4 @@ SET filename=%1
 SET outputFilename=%2
 
 :: Delete line with [ KB]        | Delete line with [ byte]| Delete line with [ bytes]| Delete line with less than 100 MB.
-sed "s/.* KB\x3C.*//" %filename% | sed "s/.* byte\x3C.*//" | sed "s/.* bytes\x3C.*//" | sed -r "s/.*\x3E[0-9][0-9]?\.?[0-9]* MB\x3C.*//" | sed -e "/^$/d"   > %outputFilename%
+sed "s/.* KB\x3C.*//" %filename% | sed "s/.* byte\x3C.*//" | sed "s/.* bytes\x3C.*//" | sed -r "s/.*\x3E[0-9][0-9]?\.?[0-9]* MB\x3C.*//" | sed "/^$/d"   > %outputFilename%
