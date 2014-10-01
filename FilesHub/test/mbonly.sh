2394d4ea66b4d878768d228368976c97a017d33c
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-10-01 16:34:31 -0400

    Update.

--- FilesHub/test/mbonly.sh
+++ FilesHub/test/mbonly.sh
@@ -2,10 +2,12 @@
 
 # Description: Display megabytes files only.
 # Author: Xuan Ngo
+# Usage: mbonly.sh inputfile outputfile
 	
 filename=$1
-sed -e 's/.* KB<.*//'  $filename | \
-sed -e 's/.* byte<.*//' | \
-sed -e 's/.* bytes<.*//' > $filename.mb.html
-
-sed -e 's/.* [1-9][0-9].[0-9][0-9] MB<.*//' $filename.mb.html > $filename.mb.100.html
+outputfilename=$2
+sed 's/.* KB<.*//'  $filename | \
+sed 's/.* byte<.*//' | \
+sed 's/.* bytes<.*//' | \
+sed -r 's/.*>[0-9][0-9]?\.?[0-9]* MB<.*//' |
+sed '/^$/d' > $outputfilename
