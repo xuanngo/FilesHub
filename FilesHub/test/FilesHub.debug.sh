08c263b5d8aa2c0fbdd69ace23e5367f22c2d644
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-10-07 07:22:26 -0400

    add.

---
+++ FilesHub/test/FilesHub.debug.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the path location of the executing script
+## http://stackoverflow.com/questions/630372/determine-the-path-of-the-executing-bash-script
+FILESHUB_HOME="`dirname \"$0\"`"                    # relative
+FILESHUB_HOME="`( cd \"$FILESHUB_HOME\" && pwd )`"  # absolutized and normalized
+if [ -z "$FILESHUB_HOME" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
+## ISO-8859-1
+java -DFilesHub.files.in.batch=11 -Dfile.encoding=UTF-8 -DFilesHub.home=${FILESHUB_HOME} -DFilesHub.debug=true -jar ${FILESHUB_HOME}/fileshub.jar "$@"
