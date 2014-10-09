23ae28420ffa414285abc1a0ec32cffdbc965ea5
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-10-09 10:56:52 -0400

    "-a" should be rock solid.

--- FilesHub/UsefulQueries.sql
+++ FilesHub/UsefulQueries.sql
@@ -25,7 +25,14 @@ WHERE    rowid NOT IN
 sqlite3 FilesHub.db "DELETE FROM Trash WHERE rowid NOT IN ( SELECT  min(rowid) FROM Trash GROUP BY duid, hash, canonical_path );"
 sqlite3 FilesHub.db -cmd ".width 6 10 5 125" -column -header "SELECT duid, hash, COUNT(*), canonical_path FROM Trash GROUP BY hash, canonical_path HAVING COUNT(*) > 1;"
          
+
+# Delete same document in Shelf and Trash
+sqlite3 FilesHub.db -cmd ".width 6 125" -column -header "SELECT Shelf.uid, Shelf.canonical_path, Trash.canonical_path FROM Shelf LEFT JOIN Trash ON Shelf.canonical_path=Trash.canonical_path WHERE Trash.canonical_path IS NOT NULL"
+sqlite3 FilesHub.db -cmd ".width 6 125" -column -header "DELETE FROM Trash WHERE canonical_path IN (SELECT Shelf.canonical_path FROM Shelf LEFT JOIN Trash ON Shelf.canonical_path=Trash.canonical_path WHERE Trash.canonical_path IS NOT NULL)"
+sqlite3 FilesHub.db -cmd ".width 6 125" -column -header "SELECT Shelf.uid, Shelf.canonical_path, Trash.canonical_path FROM Shelf LEFT JOIN Trash ON Shelf.canonical_path=Trash.canonical_path WHERE Trash.canonical_path IS NOT NULL"
+
 # Dump
 select duid, count(hash) from Trash group by hash having count(hash)>10 ;
+
 
 
