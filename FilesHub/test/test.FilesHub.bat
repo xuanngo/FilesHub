728403934c5f1b1443dfee6165bb6cb3d45322c8
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-10-20 15:22:22 -0400

    Fix extreme unit test.

--- FilesHub/test/test.FilesHub.bat
+++ FilesHub/test/test.FilesHub.bat
@@ -3,12 +3,12 @@ REM sqlite3 FilesHub.db "SELECT * FROM Shelf;"
 
 REM sqlite3 FilesHub.db "SELECT filename FROM Shelf ORDER BY filename ASC;"
 
-REM sqlite3 FilesHub.db -cmd ".width 4 15 15 125" -column -header "SELECT uid, hash, last_modified, canonical_path FROM Shelf LIMIT 100;"
+REM sqlite3 FilesHub.db -cmd ".width 11 15 15 125" -column -header "SELECT uid, hash, last_modified, canonical_path FROM Shelf LIMIT 100;"
 
-REM sqlite3 FilesHub.db -cmd ".width 4 15 15 4 125" -column -header "SELECT duid, hash, last_modified, uid, canonical_path FROM Trash LIMIT 100;"
+REM sqlite3 FilesHub.db -cmd ".width 11 15 15 4 125" -column -header "SELECT duid, hash, last_modified, uid, canonical_path FROM Trash LIMIT 100;"
 
 cls
 
-sqlite3 FilesHub.db -cmd ".width 4 15 15 125" -column -header "SELECT uid, hash, last_modified, canonical_path FROM Shelf;"
+sqlite3 FilesHub.db -cmd ".width 11 15 15 125" -column -header "SELECT uid, hash, last_modified, canonical_path FROM Shelf;"
 
-sqlite3 FilesHub.db -cmd ".width 4 15 15 4 125" -column -header "SELECT duid, hash, last_modified, uid, canonical_path FROM Trash;"
+sqlite3 FilesHub.db -cmd ".width 11 15 15 4 125" -column -header "SELECT duid, hash, last_modified, uid, canonical_path FROM Trash;"
