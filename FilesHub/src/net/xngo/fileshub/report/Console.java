eedb1e8d368047d83000d5e0ba992868118d1e1a
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-10-21 11:19:14 -0400

    printProgress() should be faster.

--- FilesHub/src/net/xngo/fileshub/report/Console.java
+++ FilesHub/src/net/xngo/fileshub/report/Console.java
@@ -3,17 +3,20 @@ package net.xngo.fileshub.report;
 public class Console
 {
   private int lastLineLength = 0;
+  private StringBuilder progressLine = new StringBuilder();
   
   public void printProgress(String line)
   {
-    // Print to console.
-    System.out.print('\r');
-    System.out.print(line);
+    this.progressLine.append('\r');
+    this.progressLine.append(line);
     
     // If last line is longer, then mask with space.
     final int spaces = lastLineLength-line.length();
     for(int i=0; i<spaces; i++)
-      System.out.print(' ');
+      this.progressLine.append(' ');
+    
+    System.out.print(this.progressLine.toString());
+    this.progressLine.setLength(0); // Reset the progress line.
     
     // Update previous line length.
     this.lastLineLength = line.length();
