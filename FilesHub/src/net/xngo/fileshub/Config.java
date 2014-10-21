d21ba09967040d1295a0008d81d43f9c9747d357
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-10-21 16:09:05 -0400

    add comment.

--- FilesHub/src/net/xngo/fileshub/Config.java
+++ FilesHub/src/net/xngo/fileshub/Config.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 public class Config
 {
+  // Mandatory.
   public static final String NAME               = "FilesHub";
   public static final String HOME_DIR           = System.getProperty(NAME+".home");
   public static final String DB_FILE_PATH       = HOME_DIR+File.separator+NAME+".db";         // File.separator might be double depending on how dbname.home system property is supplied.
@@ -12,6 +13,7 @@ public class Config
   public static final String DB_URL             = "jdbc:sqlite:/"+DB_FILE_PATH.replace('\\', '/'); // Not efficient. Use File.toURI().toURL().toString();  
   
   
+  // Optional.
   public static final String DEBUG              = System.getProperty(NAME+".debug");          // Debug mode: true or false
   public static final int FILES_IN_BATCH        = Integer.parseInt(System.getProperty(NAME+".files.in.batch")); // Number of files to process within a batch.
   public static final String WORD_LIST          = HOME_DIR+File.separator+"words.lst";
