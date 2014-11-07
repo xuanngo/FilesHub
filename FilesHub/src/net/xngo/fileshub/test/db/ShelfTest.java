c349b8e1f082ff67241f224f5d6ea80fcf22e779
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-11-07 15:26:26 -0500

    Add unit test searchDocsByFilenameUsingWildcard().

--- FilesHub/src/net/xngo/fileshub/test/db/ShelfTest.java
+++ FilesHub/src/net/xngo/fileshub/test/db/ShelfTest.java
@@ -1,13 +1,16 @@
 package net.xngo.fileshub.test.db;
 
+import java.io.File;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
+
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertTrue;
-
-import java.io.File;
-
 import net.xngo.fileshub.Main;
 import net.xngo.fileshub.Utils;
 import net.xngo.fileshub.db.Manager;
@@ -22,7 +25,7 @@ import net.xngo.fileshub.test.helpers.Data;
 
 public class ShelfTest
 {
-  private static final boolean DEBUG = false;
+  private static final boolean DEBUG = true;
   
   private Shelf shelf = new Shelf();
   
@@ -37,7 +40,7 @@ public class ShelfTest
   @Test(description="Search by filename without wildcard: Test SQL query.")
   public void searchDocsByFilenameNotWildcardQuery()
   {
-    //*** Prepare data: Add unique file in database.
+    //*** Prepare data: Add unique file in Shelf table.
     File uniqueFile = Data.createTempFile("searchDocsByFilenameNotWildcard");
     Document shelfDoc = new Document(uniqueFile);
     shelfDoc.hash = Utils.getHash(uniqueFile);
@@ -51,12 +54,15 @@ public class ShelfTest
     Connection connection = Main.connection;
     String expectedSQLquery = String.format("SELECT uid, canonical_path, filename, last_modified, hash, comment FROM Shelf WHERE filename = ? : %s", uniqueFile.getName());
     assertEquals(connection.getQueryString(), expectedSQLquery);
+    
+    //*** Clean up.
+    uniqueFile.delete();    
   }
   
   @Test(description="Search by filename with wildcard: Test SQL query.")
   public void searchDocsByFilenameWildcardQuery()
   {
-    //*** Prepare data: Add unique file in database.
+    //*** Prepare data: Add unique file in Shelf table.
     File uniqueFile = Data.createTempFile("searchDocsByFilenameWildcardQuery");
     Document shelfDoc = new Document(uniqueFile);
     shelfDoc.hash = Utils.getHash(uniqueFile);
@@ -72,12 +78,15 @@ public class ShelfTest
     Connection connection = Main.connection;
     String expectedSQLquery = String.format("SELECT uid, canonical_path, filename, last_modified, hash, comment FROM Shelf WHERE filename like ? : %s", expectedFilenameWildcard);
     assertEquals(connection.getQueryString(), expectedSQLquery);
+    
+    //*** Clean up.
+    uniqueFile.delete();    
   }
   
   @Test(description="Search by filename with multiple adjacent wildcards: Test SQL query.")
   public void searchDocsByFilenameAdjacentWildcardQuery()
   {
-    //*** Prepare data: Add unique file in database.
+    //*** Prepare data: Add unique file in Shelf table.
     File uniqueFile = Data.createTempFile("searchDocsByFilenameAdjacentWildcardQuery");
     Document shelfDoc = new Document(uniqueFile);
     shelfDoc.hash = Utils.getHash(uniqueFile);
@@ -93,6 +102,73 @@ public class ShelfTest
     Connection connection = Main.connection;
     String expectedSQLquery = String.format("SELECT uid, canonical_path, filename, last_modified, hash, comment FROM Shelf WHERE filename like ? : %s", expectedFilenameWildcard);
     assertEquals(connection.getQueryString(), expectedSQLquery);
+
+    //*** Clean up.
+    uniqueFile.delete();    
   }  
+  
+  
+  @Test(description="Search by exact filename.")
+  public void searchDocsByExactFilename()
+  {
+    //*** Prepare data: Add unique file in Shelf table.
+    File uniqueFile = Data.createTempFile("searchDocsByExactFilename");
+    Document shelfDoc = new Document(uniqueFile);
+    shelfDoc.hash = Utils.getHash(uniqueFile);
+    this.shelf.addDoc(shelfDoc);
+    
+    //*** Main test: Search by exact filename.
+    List<Document> searchResults = this.shelf.searchDocsByFilename(uniqueFile.getName());
+    
+    //*** Validations: Filename should be found.
+    assertEquals(searchResults.size(), 1, String.format("%s should be found in Shelf.", uniqueFile.getName()));
+    assertEquals(searchResults.get(0).filename, uniqueFile.getName());
+    
+    //*** Clean up.
+    uniqueFile.delete();
+  }
+  
+  @Test(description="Search by filename using wildcard.")
+  public void searchDocsByFilenameUsingWildcard()
+  {
+    if(this.DEBUG)
+    {
+      try
+      {
+        Main.connection.setAutoCommit(true);
+      }
+      catch(SQLException ex) { ex.printStackTrace(); }
+    }
+    
+    //*** Prepare data: Add multiple files in Shelf table.
+    ArrayList<File> files = new ArrayList<File>();
+    final String filenamePattern = "searchDocsByFilenameUsingWildcard_ShelfTable_";
+    for(int i=0; i<7; i++ )
+    {
+      File uniqueFile = Data.createTempFile(filenamePattern+i);
+      Document shelfDoc = new Document(uniqueFile);
+      shelfDoc.hash = Utils.getHash(uniqueFile);
+      this.shelf.addDoc(shelfDoc);
+    }
+    
+    //*** Main test: Search by exact filename.
+    List<Document> searchResults = this.shelf.searchDocsByFilename("*ByFilename*UsingWildcard*ShelfTable");
+    
+    //*** Validations: Filename should be found.
+    assertEquals(searchResults.size(), files.size(), "Number of files found in Shelf table not expected.");
+    for(int i=0; i<files.size(); i++)
+    {
+      assertNotEquals(searchResults.get(i).filename.indexOf(filenamePattern), -1, String.format("Pattern '%s' not found in %s.", filenamePattern, searchResults.get(i).filename));
+    }
+    
+    //*** Clean up.
+    for(File file: files)
+    {
+      file.delete();
+    }
+
+  } 
+  
+  
   
 }
