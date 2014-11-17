2fb2c8a6ada1d6befc859e86c67ddac197333a33
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-11-17 10:32:36 -0500

    Remove Config.FILES_IN_BATCH.

--- FilesHub/test/FilesHub.debug.bat
+++ FilesHub/test/FilesHub.debug.bat
@@ -2,4 +2,4 @@
 SET FILESHUB_HOME=%~dp0
 @ECHO ON
 ECHO [%*]
-java -DFilesHub.files.in.batch=11 -DFilesHub.home=%FILESHUB_HOME% -DFilesHub.debug=true -jar %FILESHUB_HOME%fileshub.jar %*
+java -DFilesHub.home=%FILESHUB_HOME% -DFilesHub.debug=true -jar %FILESHUB_HOME%fileshub.jar %*
