81566d3287913d54af078b420c93305d787e573b
limelime <limelime@users.noreply.github.com>
2015-03-20 17:06:15 -0400

    1st unit test for upgrade.

--- FilesHub/src/net/xngo/fileshub/test/upgrade/Version0002Test.java
+++ FilesHub/src/net/xngo/fileshub/test/upgrade/Version0002Test.java
@@ -1,14 +1,41 @@
 package net.xngo.fileshub.test.upgrade;
 
+import java.util.List;
+import java.io.File;
+
+
+
+
+import net.xngo.fileshub.Main;
+import net.xngo.fileshub.db.Connection;
+import net.xngo.fileshub.db.Shelf;
+import net.xngo.fileshub.db.Trash;
 import net.xngo.fileshub.test.helpers.FHSampleDb;
+import net.xngo.fileshub.upgrade.Upgrade;
+import net.xngo.fileshub.struct.Document;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
+
 public class Version0002Test
 {
   private FHSampleDb fhSampleDb = new FHSampleDb();
+  
+  private Shelf shelf = new Shelf();
+  private Trash trash = new Trash();  
+  
+  public Version0002Test()
+  {
+    Upgrade upgrade = new Upgrade();
+    upgrade.run();
+    
+    // Need to open connection because Flywaydb closes it after Upgrade is ran.
+    Main.connection = new Connection(); 
+  }
   
   @BeforeTest
   public void createSampleData()
@@ -22,10 +49,22 @@ public class Version0002Test
     this.fhSampleDb.deleteData();
   }
   
-  @Test(description="Test upgrade to version 2 with entries where file size is null.")
-  public void runWithFileSizeIsNull()
+  @Test(description="Test all existing files has correct file size.")
+  public void runFileSizeMatch()
   {
-
+    List<Document> shelfDocsList = this.shelf.getDocs();
+    for(Document shelfDoc: shelfDocsList)
+    {
+      File file = new File(shelfDoc.canonical_path);
+      if(file.exists())
+      {
+        long actualSize = shelfDoc.size;
+        long expectedSize = file.length();
+            
+        assertEquals(actualSize, expectedSize, shelfDoc.getInfo("File size doesn't match:"));
+      }
+      
+    }
   }
   
 }
