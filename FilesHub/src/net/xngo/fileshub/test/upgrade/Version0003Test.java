2e10a97612e00780553adae2e97fc8f571e2fe48
limelime <limelime@users.noreply.github.com>
2015-03-20 17:29:46 -0400

    Add unit test for upgrade to version 3.

---
+++ FilesHub/src/net/xngo/fileshub/test/upgrade/Version0003Test.java
@@ -0,0 +1,75 @@
+package net.xngo.fileshub.test.upgrade;
+
+import java.util.List;
+import java.io.File;
+
+
+
+
+
+import net.xngo.fileshub.Main;
+import net.xngo.fileshub.Utils;
+import net.xngo.fileshub.db.Connection;
+import net.xngo.fileshub.db.Shelf;
+import net.xngo.fileshub.db.Trash;
+import net.xngo.fileshub.test.helpers.FHSampleDb;
+import net.xngo.fileshub.upgrade.Upgrade;
+import net.xngo.fileshub.struct.Document;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class Version0003Test
+{
+  private FHSampleDb fhSampleDb = new FHSampleDb();
+  
+  private Shelf shelf = new Shelf();
+  private Trash trash = new Trash();  
+  
+  public Version0003Test()
+  {
+    Upgrade upgrade = new Upgrade();
+    upgrade.run();
+    
+    // Need to open connection because Flywaydb closes it after Upgrade is ran.
+    Main.connection = new Connection(); 
+  }
+  
+  @BeforeTest
+  public void createSampleData()
+  {
+    this.fhSampleDb.createSampleData();
+  }
+  
+  @AfterTest
+  public void deleteData()
+  {
+    this.fhSampleDb.deleteData();
+  }
+  
+  @Test(description="Test all existing files has correct MD5 hash.")
+  public void runMD5HashMatch()
+  {
+    List<Document> shelfDocsList = this.shelf.getDocs();
+    for(Document shelfDoc: shelfDocsList)
+    {
+      File file = new File(shelfDoc.canonical_path);
+      if(file.exists())
+      {
+        String actualHash = shelfDoc.hash;
+        String expectedHash = Utils.getHash(file);
+        
+        assertEquals(actualHash.length(), 32);
+        assertEquals(expectedHash.length(), 32);
+        
+        assertEquals(actualHash, expectedHash, shelfDoc.getInfo("MD5 Hash doesn't match:"));
+      }
+      
+    }
+  }
+  
+}
