9cfddb08a980ecd529063925630a6627e73b1820
limelime <limelime@users.noreply.github.com>
2015-03-26 17:38:49 -0400

    Add sql script to delete duplicate hashes.

--- FilesHub/UsefulQueries.sql
+++ FilesHub/UsefulQueries.sql
@@ -32,9 +32,13 @@ sqlite3 FilesHub.db -cmd ".width 6 125" -column -header "DELETE FROM Trash WHERE
 sqlite3 FilesHub.db -cmd ".width 6 125" -column -header "SELECT Shelf.uid, Shelf.canonical_path, Trash.canonical_path FROM Shelf LEFT JOIN Trash ON Shelf.canonical_path=Trash.canonical_path WHERE Trash.canonical_path IS NOT NULL"
 
 # Delete orphan documents: duid in Trash but not Shelf
-sqlite3 FilesHub.db -cmd ".width 6 125" -column -header "SELECT Trash.duid, Trash.canonical_path, Trash.canonical_path FROM Trash LEFT JOIN Shelf ON Trash.duid=Shelf.uid WHERE Shelf.uid IS NULL"
+sqlite3 -column -header -cmd ".width 6 125" FilesHub.db "SELECT Trash.duid, Trash.canonical_path FROM Trash LEFT JOIN Shelf ON Trash.duid=Shelf.uid WHERE Shelf.uid IS NULL"
 sqlite3 FilesHub.db "DELETE FROM Trash WHERE duid IN (SELECT Trash.duid FROM Trash LEFT JOIN Shelf ON Trash.duid=Shelf.uid WHERE Shelf.uid IS NULL)"
 
+# Delete duplicate hashes.
+sqlite3 FilesHub.db "SELECT hash, COUNT(*), canonical_path FROM Shelf GROUP BY hash HAVING COUNT(*) > 1;"
+sqlite3 FilesHub.db "DELETE FROM Shelf WHERE hash IN (SELECT hash FROM Shelf GROUP BY hash HAVING COUNT(*) > 1);"
+sqlite3 FilesHub.db "SELECT hash, COUNT(*), canonical_path FROM Shelf GROUP BY hash HAVING COUNT(*) > 1;"
 
 # Find the longest canonical_path
 sqlite3 FilesHub.db "select length(canonical_path), * from Shelf order by length(canonical_path) desc limit 1;" | wc -c
