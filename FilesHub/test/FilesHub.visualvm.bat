c00a6efa3dba15ef500738447693a896224c5a97
limelime <limelime@users.noreply.github.com>
2015-03-26 13:42:07 -0400

    Add hash.frequency.

--- FilesHub/test/FilesHub.visualvm.bat
+++ FilesHub/test/FilesHub.visualvm.bat
@@ -2,4 +2,6 @@
 SET FILESHUB_HOME=%~dp0
 @ECHO ON
 ECHO [%*]
-java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=6677 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false -Djava.rmi.server.hostname=localhost -DFilesHub.home=%FILESHUB_HOME% -DFilesHub.debug=true -jar %FILESHUB_HOME%fileshub.jar %*
+::java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=6677 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false -Djava.rmi.server.hostname=localhost -DFilesHub.home=%FILESHUB_HOME% -DFilesHub.debug=true -jar %FILESHUB_HOME%fileshub.jar %*
+
+java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=6677 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false -Djava.rmi.server.hostname=localhost -DFilesHub.home=%FILESHUB_HOME% -DFilesHub.hash.frequency=13 -jar %FILESHUB_HOME%fileshub.jar %*
