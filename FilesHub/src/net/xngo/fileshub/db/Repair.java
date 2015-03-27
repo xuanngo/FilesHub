f8ab93ce2f9d3a90e64707d09579a0b53fe82caf
limelime <limelime@users.noreply.github.com>
2015-03-27 15:10:51 -0400

    update.

--- FilesHub/src/net/xngo/fileshub/db/Repair.java
+++ FilesHub/src/net/xngo/fileshub/db/Repair.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Repair database:
+ *    - Delete orphans document in Trash table.
  *    - Check hash is correct: 32 chars long and hexadecimal.
  * @author root
  *
@@ -13,15 +14,25 @@ public class Repair
 {
   final static Logger log = LoggerFactory.getLogger(Repair.class);
   
+  private Trash trash = new Trash();
+  
   public void commit(boolean commit)
   {
     if(commit)
     {
+      String orphanMsg = String.format("%d orphan documents deleted.", this.trash.removeOrphans());
+      
       System.out.println("Commit");
+      System.out.println(orphanMsg);
     }
     else
     {
+      String orphanMsg = String.format("%d orphan documents to be deleted.", this.trash.getTotalOrphans());
+      
       System.out.println("Simulation");
+      System.out.println(orphanMsg);
     }
   }
+  
+  
 }
