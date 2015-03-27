191b4cf0c8b7b5a9fd9993f64a909962b95f5813
limelime <limelime@users.noreply.github.com>
2015-03-27 11:31:44 -0400

    Create CmdRepair class.

---
+++ FilesHub/src/net/xngo/fileshub/cmd/CmdRepair.java
@@ -0,0 +1,17 @@
+package net.xngo.fileshub.cmd;
+
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+
+@Parameters(commandDescription = "Search by different attributes.")
+public class CmdRepair
+{
+  public static String name = "repair";
+  
+  @Parameter(names = {"-c", "--commit"}, description = "True to commit repair. "
+                                                  + "Otherwise, list all actions to be executed without committing them.", arity = 1)
+  public boolean commit=false;
+  
+}
