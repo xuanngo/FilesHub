1620f36fa6899d5a9eed0e1f5ede2a4348a57680
limelime <limelime@users.noreply.github.com>
2015-03-27 17:27:00 -0400

    Add unit tests for getHash().

--- FilesHub/src/net/xngo/fileshub/test/UtilsTest.java
+++ FilesHub/src/net/xngo/fileshub/test/UtilsTest.java
@@ -2,6 +2,7 @@ package net.xngo.fileshub.test;
 
 import java.io.File;
 
+import net.xngo.fileshub.Config;
 import net.xngo.fileshub.Utils;
 import net.xngo.fileshub.test.helpers.Data;
 import net.xngo.utils.java.math.Hash;
@@ -11,15 +12,16 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
 import static org.testng.Assert.assertTrue;
 
 public class UtilsTest
 {
   @Test(description="getHash(): Small file, less than 4MB.")
-  public void getHashSmallFile()
+  public void getHashSmallFileLessThan4MB()
   {
     //*** Prepare data: Create a small file, less than 4MB.
-    File uniqueFile = Data.createTempFile("getHashSmallFile");
+    File uniqueFile = Data.createTempFile("getHashSmallFileLessThan4MB");
     
     //*** Test: Utils.getHash() should return the same hash as Hash.md5().
     String actualHash = Utils.getHash(uniqueFile);
@@ -29,4 +31,20 @@ public class UtilsTest
     //*** Clean up.
     uniqueFile.delete();    
   }
+  
+  @Test(description="getHash(): Big file, more than 4MB.")
+  public void getHashBigFileMoreThan4MB()
+  {
+    //*** Prepare data: Create a big file, more than 4MB.
+    File uniqueFile = Data.createTempFileWithByte("getHashBigFileMoreThan4MB", Data.getRandomBytes(4*1024*1024+1));
+    
+    //*** Test: Utils.getHash() should return the same hash as Hash.md5().
+    String actualHash = Utils.getHash(uniqueFile);
+    String md5Hash = Hash.md5(uniqueFile);
+    assertNotEquals(actualHash, md5Hash, String.format("Hash should be different for %s.", uniqueFile.getAbsolutePath()));
+    
+    //*** Clean up.
+    //uniqueFile.delete();    
+  }
+  
 }
