b163df0dee19c05ca2c61f479ae1e211e1b50538
limelime <limelime@users.noreply.github.com>
2015-03-30 17:27:27 -0400

    :: To handle path with spaces, you have to: ::   -Change backward slash to forward slash ::   -Add double quotes

--- FilesHub/releases/latest/fh.bat
+++ FilesHub/releases/latest/fh.bat
@@ -7,4 +7,10 @@ REM ISO-8859-1
 :: -DFilesHub.hash.frequency: It defines the number of times to spot hash a file when its size is greater than 4 MB.
 ::                            0 means hash the whole file.
 
-java -DFilesHub.home=%FILESHUB_HOME% -Dlogback.configurationFile=%FILESHUB_HOME%logback.xml -DFilesHub.hash.frequency=13 -jar %FILESHUB_HOME%Fileshub.jar %*
+:: To handle path with spaces, you have to:
+::   -Change backward slash to forward slash
+::   -Add double quotes
+SET FILESHUB_HOME=%FILESHUB_HOME:\=/%
+SET FILESHUB_HOME_QUOTED="%FILESHUB_HOME%"
+
+java -DFilesHub.home=%FILESHUB_HOME_QUOTED% -Dlogback.configurationFile=logback.xml -DFilesHub.hash.frequency=13 -jar "%FILESHUB_HOME%Fileshub.jar" %*
