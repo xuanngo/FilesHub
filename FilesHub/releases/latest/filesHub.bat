f882f69b88a4aa753ccc9fd89760049c06fd1036
limelime <limelime@users.noreply.github.com>
2015-03-30 09:48:58 -0400

    Rename executable with better naming.

--- FilesHub/releases/latest/filesHub.bat
+++ FilesHub/releases/latest/filesHub.bat
@@ -7,4 +7,4 @@ REM ISO-8859-1
 :: -DFilesHub.hash.frequency: It defines the number of times to spot hash a file when its size is greater than 4 MB.
 ::                            0 means hash the whole file.
 
-java -DFilesHub.home=%FILESHUB_HOME% -Dlogback.configurationFile=%FILESHUB_HOME%logback.xml -DFilesHub.hash.frequency=13 -jar %FILESHUB_HOME%fileshub.jar %*
+java -DFilesHub.home=%FILESHUB_HOME% -Dlogback.configurationFile=%FILESHUB_HOME%logback.xml -DFilesHub.hash.frequency=13 -jar %FILESHUB_HOME%Fileshub.jar %*
