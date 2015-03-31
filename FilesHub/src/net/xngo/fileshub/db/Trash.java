e6002e790b79682cf0022bd239966ad504fd6fc3
limelime <limelime@users.noreply.github.com>
2015-03-31 10:22:03 -0400

    Remove hypen, tab is sufficient.

--- FilesHub/src/net/xngo/fileshub/db/Trash.java
+++ FilesHub/src/net/xngo/fileshub/db/Trash.java
@@ -404,7 +404,7 @@ public class Trash
     {
       StringBuffer msg = new StringBuffer(String.format("'%s' returns %d entries with same hash. Expect 0 or 1 entry. Entries found are:\n", Main.connection.getQueryString(), docs.size()));
       for(Document doc: docs)
-        msg.append("\t-").append(doc.canonical_path).append("\n");
+        msg.append("\t").append(doc.canonical_path).append("\n");
       
       RuntimeException rException = new RuntimeException(msg.toString());
       log.error("This method can't return 2 or more documents with the same hash.", rException);
