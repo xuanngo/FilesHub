63863c97b8eea245db5b2d711cf8a69cbd80ca9d
limelime <limelime@users.noreply.github.com>
2015-04-01 13:55:26 -0400

    Fix options position.

--- FilesHub/test/test.FilesHub.sh
+++ FilesHub/test/test.FilesHub.sh
@@ -3,11 +3,11 @@
 clear 
 
 echo "Shelf:"
-sqlite3 FilesHub.db -cmd ".width 11 15 15 10 125" -column -header "SELECT uid, hash, last_modified, size, canonical_path FROM Shelf;"
+sqlite3 -cmd ".width 11 15 15 10 125" -column -header FilesHub.db "SELECT uid, hash, last_modified, size, canonical_path FROM Shelf;"
 
 echo "Trash:"
-sqlite3 FilesHub.db -cmd ".width 11 15 15 10 4 125" -column -header "SELECT duid, hash, last_modified, size, uid, canonical_path FROM Trash;"
+sqlite3 -cmd ".width 11 15 15 10 4 125" -column -header FilesHub.db "SELECT duid, hash, last_modified, size, uid, canonical_path FROM Trash;"
 
 echo ""
 echo "Schema:"
-sqlite3 FilesHub.db -cmd ".width 13 15 11 25 11 25 11 13 24 15 11" -column -header "SELECT * from schema_version;"
+sqlite3 -cmd ".width 13 15 11 25 11 25 11 13 24 15 11" -column -header FilesHub.db "SELECT * from schema_version;"
