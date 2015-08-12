7005bdc19ef9fe0aa9a421688b4e3ac508e12647
limelime <limelime@users.noreply.github.com>
2015-08-12 17:38:57 -0400

    Add comment.

--- test/test.view.jar.files.sh
+++ test/test.view.jar.files.sh
@@ -1,19 +1,28 @@
 # View all files in fileshub.jar
 FILESHUB_JAR=Fileshub.jar
-FILESHUB_TMP_DIR=fileshub
+FILESHUB_TMP_DIR=fh_extracted
 
 if [ ! -f ${FILESHUB_JAR} ]
 then
   echo "ERROR: ${FILESHUB_JAR} is not found!"
 else
-
+  
+  # Clean the temporary folder.
   rm -rf ${FILESHUB_TMP_DIR}
 	mkdir ${FILESHUB_TMP_DIR}
+	
+	# Copy jar to temporary folder.
 	cp ${FILESHUB_JAR} ${FILESHUB_TMP_DIR}
+	
+	# Extract jar file. 
 	cd ${FILESHUB_TMP_DIR}
 	jar xf ${FILESHUB_JAR}
+	
+	# Delete jar file.
 	rm -f ${FILESHUB_JAR}
 	cd ..
+	
+	# List all files in the jar file
 	clear
 	find ${FILESHUB_TMP_DIR} -type f | sort
             
